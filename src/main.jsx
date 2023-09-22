d1662e6e02e9d051621e5f742f2e147ff2c6ed32
Dibbo T <dibbotripura7@gmail.com>
2023-09-22 09:01:30 +0600

    navbar created

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Home from './components/Home'
 import './index.css'
+
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Root from './components/Root';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
